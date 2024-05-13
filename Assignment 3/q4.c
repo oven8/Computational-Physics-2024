e6095c798cd9b5abbc5f94ca51c78f7534da4483
oven8 <sambitsarkar2012@gmail.com>
2024-05-13 07:34:04 +0530

    Q4 Update

---
+++ Assignment 3/q4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <fftw3.h>
+
+#define N 1001
+#define xlim 50
+
+double sinc(double x) {
+    return exp(-pow(x,2));
+}
+
+int main() {
+    fftw_complex in[N], out[N];
+    fftw_plan p;
+    double Delta = 2 * xlim / (N - 1);
+
+    for (int i=0; i < N; i++) {
+        double x = -xlim + i * Delta;
+        in[i] = sinc(x) + I * 0.0;
+    }
+
+    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    fftw_execute(p);
+
+    FILE *file = fopen("q4_data.csv","w");
+    if (file == NULL) {
+        printf("Error Opening File!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        fprintf(file, "%g, %g\n", creal(out[i]), cimag(out[i]));
+    }
+
+    fclose(file);
+
+    fftw_destroy_plan(p);
+    fftw_cleanup();
+
+    printf("Fourier transform data has been written to 'q4_data.csv'.\n");
+
+    return 0;
+}
